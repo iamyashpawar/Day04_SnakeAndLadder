cc3eee3dde2be97c4694711e4eb474e9db4b0a6f
Yash <imyashpawar@gmail.com>
2023-04-08 17:28:10 +0530

    Welcome to Snake and Ladder

---
+++ src/com/bridgelabz/snakeAndLadder.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class snakeAndLadder
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Snake and Ladder Simulation");
+    }
+}
